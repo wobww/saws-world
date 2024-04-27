147455b098fd233d6413d8c18ec90e9a31e08e01
Rob <23304286+WobWainwwight@users.noreply.github.com>
2024-04-27 10:45:02 +0100

    Add immutable cache control directive

--- cmd/main.go
+++ cmd/main.go
@@ -342,7 +342,7 @@ func main() {
 			return
 		}
 
-		w.Header().Add("Cache-Control", "private,max-age=2628288")
+		w.Header().Add("Cache-Control", "private, max-age=2628288, immutable")
 		w.Write(fileBytes)
 	})))
 
